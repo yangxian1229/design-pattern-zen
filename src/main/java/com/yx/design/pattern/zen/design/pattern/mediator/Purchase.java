4f5a1e7a8d417bb8742542aab467f2da93c03368
yangxian007 <yangxian007@ke.com>
2024-02-21 11:10:15 +0800

    中介者模式-进销存示例-依赖方法和自有方法

--- src/main/java/com/yx/design/pattern/zen/design/pattern/mediator/Purchase.java
+++ src/main/java/com/yx/design/pattern/zen/design/pattern/mediator/Purchase.java
@@ -11,11 +11,16 @@ public class Purchase extends AbstractColleague{
     }
 
     // 采购IBM电脑
+    // 依赖方法 dep-method
     public void buyIBMcomputer(int number){
+        // 处理自己的业务逻辑
+        System.out.println("Purchase采购IBM电脑"+number+"台");
+        // 自己不能处理的业务逻辑，交给中介者处理
         super.mediator.execute("purchase.buy", number);
     }
 
     // 不再采购IBM电脑
+    // 自有方法 self-method
     public void refuseBuyIBM(){
         System.out.println("不再采购IBM电脑");
     }
